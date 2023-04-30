8fd5e56de7c0b7396d9d480a9518065c6818bc3d
vicriesp <vicriesp@hotmail.com>
2023-04-29 23:43:44 -0600

    Principles

---
+++ lesson2/scripts/myscript.js
@@ -0,0 +1,5 @@
+var today = new Date();
+var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+document.getElementById("currentdate").textContent = dateTime;
