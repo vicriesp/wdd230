5c5f65374a8006be3486a56f16d7787699938f1d
vicriesp <vicriesp@hotmail.com>
2023-05-13 22:49:18 -0600

    Comerce project

---
+++ chamber/scripts/myscripts.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+    document.getElementById('prim-nav').classList.toggle('open');
+}
+const x = document.getElementById('btn');
+x.onclick = toggleMenu;
+
+
+/*const datefield = document.querySelector("time");
+const datefieldUK = document.querySelector("aside");
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+datefield.textContent = fulldate;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;*/
+
+
+
+
+
+var today = new Date();
+var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+document.getElementById("currentdate").textContent = dateTime;
