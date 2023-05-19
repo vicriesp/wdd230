862125d61b5c8753d01364beece1cceee5b71731
vicriesp <vicriesp@hotmail.com>
2023-05-18 22:45:42 -0600

    B of M

--- lesson5/scripts/myscripts.js
+++ lesson5/scripts/myscripts.js
@@ -26,5 +26,9 @@ function init() {
 }
 
 window.addEventListener('DOMContentLoaded', init);
-/*document.getElementById('modify').innerHTML = document.lastModified
-document.getElementById('year').innerHTML = date().getFullYear()*/
+
+var today = new Date();
+var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+document.getElementById("currentdate").textContent = dateTime;
