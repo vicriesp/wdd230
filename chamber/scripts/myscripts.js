efb67b17a2794dfcccbf5a40f4db642a8a99c6e1
vicriesp <vicriesp@hotmail.com>
2023-05-20 11:45:45 -0600

    date

--- chamber/scripts/myscripts.js
+++ chamber/scripts/myscripts.js
@@ -14,13 +14,9 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", { dateStyle: "full"}).format
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
 
-
-
-
 var today = new Date();
 var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
 document.getElementById("currentdate").textContent = dateTime;
 
-document.getElementById("date").textContent = date;
