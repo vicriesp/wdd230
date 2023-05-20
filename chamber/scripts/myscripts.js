cc2b6956dd38aabc953101fec1d74b6852ee84cc
vicriesp <vicriesp@hotmail.com>
2023-05-20 10:03:30 -0600

    Grid - Flex

--- chamber/scripts/myscripts.js
+++ chamber/scripts/myscripts.js
@@ -23,3 +23,5 @@ var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
 document.getElementById("currentdate").textContent = dateTime;
+
+document.getElementById("date").textContent = date;
