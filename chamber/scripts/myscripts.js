252571fd6bb70cd2822cf80e0281abd0aebd90a5
vicriesp <vicriesp@hotmail.com>
2023-05-20 11:26:01 -0600

    link-date

--- chamber/scripts/myscripts.js
+++ chamber/scripts/myscripts.js
@@ -6,12 +6,12 @@ const x = document.getElementById('btn');
 x.onclick = toggleMenu;
 
 
-
+const datefield = document.querySelector("time");
 const datefieldUK = document.querySelector("aside");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 const fulldateUK = new Intl.DateTimeFormat("en-UK", { dateStyle: "full"}).format(now);
-datefield.textContent = fulldate;
+
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
 
