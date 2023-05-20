ce58a8af58fc61d274a96ac4185934ac915ed812
vicriesp <vicriesp@hotmail.com>
2023-05-20 11:28:34 -0600

    date

--- chamber/scripts/myscripts.js
+++ chamber/scripts/myscripts.js
@@ -11,7 +11,6 @@ const datefieldUK = document.querySelector("aside");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 const fulldateUK = new Intl.DateTimeFormat("en-UK", { dateStyle: "full"}).format(now);
-
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
 
