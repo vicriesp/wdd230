24d29ea6d0b139ee47612b01a35225ae7b5bd63d
vicriesp <vicriesp@hotmail.com>
2023-07-19 00:06:18 -0600

    bountiful

--- bountifulFoodFp/scripts/chill.js
+++ bountifulFoodFp/scripts/chill.js
@@ -4,7 +4,11 @@ const windSpeed = document.querySelector('.speed');
 const captionDesc = document.querySelector('figcaption');
 //const getwindChill = document.querySelector('.chill');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=imperial&appid=31a6ffddbf95f97b503e94dbfa753530';
+// const url = 'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=imperial&appid=31a6ffddbf95f97b503e94dbfa753530';
+const apiKey = '31a6ffddbf95f97b503e94dbfa753530';
+const lat = '33.158092';
+const lon = '-117.350594';
+const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
 async function apiFetch() {
     try {
@@ -19,28 +23,47 @@ async function apiFetch() {
     } catch (error) {
         console.log(error);
     }
-    function displayResults(weatherData) {
-        currentTemp.innerHTML = weatherData.main.temp.toFixed(0);
-        windSpeed.innerHTML = weatherData.wind.speed;
-        
-        const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-        const desc = weatherData.weather[0].description;
-        weatherIcon.setAttribute('src', iconsrc);
-        weatherIcon.setAttribute('alt', desc);
-        captionDesc.innerHTML = desc;
+    function displayResults(data) {
+        let html = '';
+        let count = 0;
+    
+        data.list.forEach(weatherData => {
+            if (count >= 3) {
+                return;
+            }
+            const temperature = weatherData.main.temp.toFixed(0);
+            const windSpeed = weatherData.wind.speed;
+            const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+            const humidity = weatherData.main.humidity;
+            const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+            const description = weatherData.weather[0].description;
+
+            html += `<tr><td><img src="${iconsrc}" alt="" id="weather-icon" /></td>`;
+            html += '<td>' + temperature + '</td>';
+            html += '<td>' + windSpeed + '</td>';
+            html += '<td>' + windChill.toFixed(2) + '</td>';
+            html += '<td>' + humidity + '</td>';
+            html += '<td>' + description + '</td>';
+            html += '</tr>';
+
+            count++;
+        });
+
+        document.querySelector('#forecast>tbody').innerHTML = html;
     }
 
-  const getWindChill = document.querySelector('.chill');
-  const temperature = currentTemp; // Replace with the actual air temperature in Fahrenheit
-  const windChill = windSpeed; // Replace with the actual wind speed in miles per hour
-  const windChillT = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windChill, 0.16) + 0.4275 * temperature * Math.pow(windChill, 0.16);
+//   const getWindChill = document.querySelector('.chill');
+//   const temperature = currentTemp; // Replace with the actual air temperature in Fahrenheit
+//   const windChill = windSpeed; // Replace with the actual wind speed in miles per hour
+//   const windChillT = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windChill, 0.16) + 0.4275 * temperature * Math.pow(windChill, 0.16);
 
   
-    if (temperature <= 50 && windChill > 3) {
-        getWindChill.innerHTML =  windChillT;
-      } 
-      else {
-        getWindChill.innerHTML =  'N/A';
-      }   
-  }
+//     if (temperature <= 50 && windChill > 3) {
+//         getWindChill.innerHTML =  windChillT;
+//       } 
+//       else {
+//         getWindChill.innerHTML =  'N/A';
+//       }   
+//   }
+}
   apiFetch();
--- bountifulFoodFp/scripts/fruits.js
+++ bountifulFoodFp/scripts/fruits.js
@@ -1,57 +1,71 @@
-const url = 'https://brotherblazzard.github.io/canvas-content/fruit.json';
+const url = 'https://vicriesp.github.io/wdd230/bountifulFoodFp/fruits.json';
 getFruitsData();
+const fruitList = [];
+const fruitInfo = {};
 
 async function getFruitsData() {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
+    //console.table(data.fruits);  // note that we reference the prophet array of the data object given the structure of the json file
     displayFruits(data.fruits);
   }
-  
-
 
   const displayFruits = (fruits) => {
-    const cards = document.querySelector('label.fruits'); // select the output container element
+    let html = "";
   
     fruits.forEach((fruit) => {
-      // Create elements to add to the div.cards element
-      let card = document.createElement('section');
-      let name = document.createElement('p');
-      //let nutritions = document.createElement('p');
-      //let address = document.createElement('p');
-      //let phonenumber = document.createElement('p');
-      //let website = document.createElement('p');
-      //let membershiplevel = document.createElement('p');
-      //let image = document.createElement('img');
-  
-      // Build the h2 content out to show the prophet's full name - finish the template string
-      p.textContent = `${fruit.name}`;
-      //address.textContent = `Address: ${fruit.address}`;
-      //image.imageurl = busin.imageurl;
-      //phonenumber.textContent = `Phone Number: ${fruit.phonenumber}`;
-      //website.textContent = `Website: ${busin.website}`;
-      //membershiplevel.textContent = `Membership Level: ${busin.membershiplevel}`;
-
-      card.setAttribute('class', 'card');
-      p.setAttribute('class', 'names')
-
-      // Build the image portrait by setting all the relevant attribute
-      //image.setAttribute('class', 'im');
-      //image.setAttribute('src', busin.imageurl);
-      //image.setAttribute('alt', `Logo of ${busin.name}`);
-      //image.setAttribute('loading', 'lazy');
-      //image.setAttribute('width', '340');
-      //image.setAttribute('height', '440');
-  
-      // Append the section(card) with the created elements
-      //card.appendChild(image);
-      //card.appendChild(p);
-      card.appendChild(name);
-      //card.appendChild(address);
-      //card.appendChild(phonenumber);
-      //card.appendChild(website);
-      //card.appendChild(membershiplevel);
-     
-      fruits.appendChild(card);
+        html += `<p><label class="sbs"><input type="checkbox" name="${fruit.name}" onchange="onFruitChange(this.name, this.checked)">${fruit.name}<span class="fruits"></span></label></p>`;
+        fruitInfo[fruit.name] = {
+            calories: fruit.calories,
+            carbohydrates: fruit.carbohydrates,
+            fat: fruit.fat,
+            sugar: fruit.sugar,
+            protein: fruit.protein,
+        };
     }); // end of forEach loop
+
+    const fruitOptions = document.getElementById('fruit-options');
+    fruitOptions.innerHTML = `<section>${html}</section>`;
   } // end of function expression
+
+  const onNameChange = (name) => {
+    document.getElementById('order-name').textContent = name;
+  };
+  const onEmailChange = (email) => {
+    document.getElementById('order-email').textContent = email;
+  };
+  const onCpnumberChange = (cpnumber) => {
+    document.getElementById('order-cpnumber').textContent = cpnumber;
+  };
+  const onDescriptionChange = (description) => {
+    document.getElementById('order-description').textContent = description;
+  };
+  const onFruitChange = (fruit, checked) => {
+    const pos = fruitList.indexOf(fruit);
+    let totalFat = 0, totalCarbs = 0, totalSugar = 0, totalProtein = 0, totalCalories = 0;
+
+    if (checked) {
+        if (pos == -1)
+            fruitList.push(fruit);
+    } else {
+        fruitList.splice(pos, 1);
+    }
+
+    fruitList.forEach(fruit => {
+        const info = fruitInfo[fruit];
+        totalFat += info.fat;
+        totalCarbs += info.carbohydrates;
+        totalProtein += info.protein;
+        totalCalories += info.calories;
+        totalSugar += info.sugar;
+    });
+
+    document.getElementById('order-date').textContent = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
+    document.getElementById('order-fruit').textContent = fruitList.join(", ");
+
+    document.getElementById('order-fat').textContent = totalFat;
+    document.getElementById('order-carbohydrates').textContent = totalCarbs;
+    document.getElementById('order-protein').textContent = totalProtein;
+    document.getElementById('order-calories').textContent = totalCalories;
+    document.getElementById('order-sugar').textContent = totalSugar;
+};
